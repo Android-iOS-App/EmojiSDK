48b42838abea0c8e79824aaba001884cb96c1763
Android-iOS-App <Android_iOS.App@yahoo>
2016-04-21 08:42:14 +0800

    EmojiSDK

---
+++ MakemojiSDK/MEMessageView.h
@@ -0,0 +1,23 @@
+//
+//  MEMessageView.h
+//  MakemojiSDK
+//
+//  Created by iOS Developer on 3/7/16.
+//  Copyright © 2016 Makemoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEMessageView : UIView
+
+@property (readonly) UIView * textContentView;
+@property (readonly) NSAttributedString * attributedString;
+
+- (void)setHTMLString:(NSString *)html;
+
+// estimate the width of a view with HTML constrained to width.
+- (CGSize)suggestedFrameSizeToFitEntireStringConstraintedToWidth:(CGFloat)width;
+- (CGSize)suggestedSizeForTextForSize:(CGSize)size;
+- (CGSize)intrinsicContentSize;
+
+@end
